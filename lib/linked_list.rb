44e67bf253676643121136e2474a10180270dc89
shawndcarpenter <shawncarpenter.co@gmail.com>
2024-03-26 10:34:36 -0600

    Modify class methods to account for 2 nodes in linked list

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,23 +1,33 @@
 require "./lib/node"
 
 class LinkedList
-  attr_reader :head
+  attr_reader :head, :next_node
 
   def initialize
     @head = nil
   end
 
   def append(name)
-    @head = Node.new(name)
+    if @head.nil?
+      @head = Node.new(name)
+    else
+      @head.next_node = Node.new(name)
+    end
   end
 
   def count
-    if !head.nil?
-      return 1
+    if head.next_node.nil?
+      1
+    else
+      2
     end
   end
 
   def to_string
-    "The #{@head.surname} family"
+    if @head.next_node.nil?
+      "The #{@head.surname} family"
+    else
+      "The #{@head.surname} family, followed by the #{@head.next_node.surname} family"
+    end
   end
 end
